7932a8a296186a9bf9206bf0766d1e51554f61ac
jocimarjsc <jocimarjsc@yahoo.com.br>
2021-09-12 18:03:40 -0300

    refactoring code 2

--- src/services/getRacking.ts
+++ src/services/getRacking.ts
@@ -7,9 +7,9 @@ class getRacking {
         const rackings = data.sort(orderDescByDate);
         const racking = `
         <b>🏆 Rancking 2021 🏆</b>
-------------------------------------------- \n ${rackings.map(team => {
+-------------------------------------- \n ${rackings.map(team => {
             return `<b>${team.place} - ${team.team.name}</b>\n ${team.change === 0 ? "Se manteve na posição: " + team.change + " ⚪": team.change > 0 ? "Subiu: " + team.change + " 🟢":"Desceu: " + team.change + " 🔴"}
-------------------------------------------- \n`
+-------------------------------------- \n`
         }).join(" ")}
         `
         return racking;
